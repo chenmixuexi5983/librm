0fd6e8f9ec0067b21a217701e03ce5ff9bcf916d
lunarifish <messenger@lunariio.work>
2024-11-10 16:40:18 +0800

    style: 清理了一下

--- src/librm/device/actuator/unitree_motor.cc
+++ src/librm/device/actuator/unitree_motor.cc
@@ -1,56 +1,110 @@
+/*
+  Copyright (c) 2024 XDU-IRobot
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+/**
+ * @file  librm/device/actuator/unitree_motor.cc
+ * @brief 宇树电机类库
+ */
 
 #include "unitree_motor.hpp"
+
 #include "librm/hal/serial_interface.h"
-#include "librm/hal/spi_interface.h"
-#include "librm/modules/algorithm/crc32.h"
+#include "librm/modules/algorithm/crc.h"
 
 using namespace rm::device;
 
+/**
+ * @param[in]      serial     串口对象
+ * @param[in]      motor_id   电机ID
+ * @returns        None
+ */
 UnitreeMotor::UnitreeMotor(hal::SerialInterface &serial, u8 motor_id) : serial_(&serial) {
   static hal::SerialRxCallbackFunction rx_callback =
       std::bind(&UnitreeMotor::RxCallback, this, std::placeholders::_1, std::placeholders::_2);
-  this->serial_->AttachRxCallback(rx_callback);
+  serial_->AttachRxCallback(rx_callback);
 
   send_data_.head.motor_id = motor_id;
 }
 
+/**
+ * @brief          设置电机力矩
+ * @param[in]      tau    力矩
+ * @returns        None
+ */
 void UnitreeMotor::SetTau(f32 tau) {
-  this->ctrl_param_.tau = tau;
-  this->ctrl_param_.vel = 0;
-  this->ctrl_param_.pos = 0;
-  this->ctrl_param_.kp = 0;
-  this->ctrl_param_.kd = 0;
+  ctrl_param_.tau = tau;
+  ctrl_param_.vel = 0;
+  ctrl_param_.pos = 0;
+  ctrl_param_.kp = 0;
+  ctrl_param_.kd = 0;
 
-  SetParam(this->ctrl_param_); 
+  SetParam(ctrl_param_);
 }
 
-void UnitreeMotor::SendCommend() { this->serial_->Write(tx_buffer_, sizeof(tx_buffer_)); }
+/**
+ * @brief          发送电机控制指令
+ * @returns        None
+ */
+void UnitreeMotor::SendCommend() { serial_->Write(tx_buffer_, sizeof(tx_buffer_)); }
 
+/**
+ * @brief          串口接收完成回调函数，解包电机发回来的反馈数据
+ * @note           不要手动调用
+ * @param[in]      data    串口接收到的数据
+ * @param[in]      rx_len  数据长度
+ * @returns        None
+ */
 void UnitreeMotor::RxCallback(const std::vector<u8> &data, u16 rx_len) {
-  if (rx_len != 78) { return; }
+  if (rx_len != 78) {
+    return;
+  }
 
   std::copy(data.begin(), data.end(), reinterpret_cast<u8 *>(&recv_data_));
 
-  this->fb_param_.mode = recv_data_.data.mode;
-  this->fb_param_.temp = recv_data_.data.temp;
-  this->fb_param_.m_error = recv_data_.data.m_error;
+  fb_param_.mode = recv_data_.data.mode;
+  fb_param_.temp = recv_data_.data.temp;
+  fb_param_.m_error = recv_data_.data.m_error;
 
-  this->fb_param_.tau = (f32)((f32)recv_data_.data.tau / 256.0f);
-  this->fb_param_.vel = (f32)((f32)recv_data_.data.vel / 128.0f);
+  fb_param_.tau = (f32)((f32)recv_data_.data.tau / 256.0f);
+  fb_param_.vel = (f32)((f32)recv_data_.data.vel / 128.0f);
 
-  this->fb_param_.acc = (i16)recv_data_.data.acc;
-  this->fb_param_.pos = (f32)((f32)recv_data_.data.pos * 6.2832f / 16384.0f);
+  fb_param_.acc = (i16)recv_data_.data.acc;
+  fb_param_.pos = (f32)((f32)recv_data_.data.pos * 6.2832f / 16384.0f);
 
-  this->fb_param_.gyro[0] = (f32)(((f32)recv_data_.data.gyro[0]) * 0.00107993176f);
-  this->fb_param_.gyro[1] = (f32)(((f32)recv_data_.data.gyro[1]) * 0.00107993176f);
-  this->fb_param_.gyro[2] = (f32)(((f32)recv_data_.data.gyro[2]) * 0.00107993176f);
+  fb_param_.gyro[0] = (f32)(((f32)recv_data_.data.gyro[0]) * 0.00107993176f);
+  fb_param_.gyro[1] = (f32)(((f32)recv_data_.data.gyro[1]) * 0.00107993176f);
+  fb_param_.gyro[2] = (f32)(((f32)recv_data_.data.gyro[2]) * 0.00107993176f);
 
-  this->fb_param_.accel[0] = (f32)(((f32)recv_data_.data.accel[0]) * 0.0023911132f);
-  this->fb_param_.accel[1] = (f32)(((f32)recv_data_.data.accel[1]) * 0.0023911132f);
-  this->fb_param_.accel[2] = (f32)(((f32)recv_data_.data.accel[2]) * 0.0023911132f);
+  fb_param_.accel[0] = (f32)(((f32)recv_data_.data.accel[0]) * 0.0023911132f);
+  fb_param_.accel[1] = (f32)(((f32)recv_data_.data.accel[1]) * 0.0023911132f);
+  fb_param_.accel[2] = (f32)(((f32)recv_data_.data.accel[2]) * 0.0023911132f);
 }
 
-void UnitreeMotor::SetParam(const control_param &ctrl_param) {
+/**
+ * @brief          设置电机控制参数
+ * @param[in]      ctrl_param    控制参数
+ * @returns        None
+ */
+void UnitreeMotor::SetParam(const ControlParam &ctrl_param) {
   send_data_.data.mode = 10;
   send_data_.data.modify_bit = 0xFF;
   send_data_.data.read_bit = 0x0;
@@ -65,7 +119,7 @@ void UnitreeMotor::SetParam(const control_param &ctrl_param) {
   send_data_.data.LowHzMotorCmdIndex = 0;
   send_data_.data.LowHzMotorCmdByte = 0;
 
-  send_data_.crc.ku32 = crc32_core((u32 *)(&send_data_), 7);
+  send_data_.crc.ku32 = modules::algorithm::Crc32((u32 *)(&send_data_), 7, modules::algorithm::CRC32_INIT);
 
   std::copy(reinterpret_cast<u8 *>(&send_data_), reinterpret_cast<u8 *>(&send_data_) + sizeof(send_data_), tx_buffer_);
 }
--- src/librm/device/actuator/unitree_motor.hpp
+++ src/librm/device/actuator/unitree_motor.hpp
@@ -23,6 +23,7 @@
 /**
  * @file  librm/device/actuator/unitree_motor.hpp
  * @brief 宇树电机类库
+ * @todo  补充注释
  */
 
 #ifndef LIBRM_DEVICE_ACTUATOR_UNITREE_MOTOR_HPP
@@ -30,119 +31,119 @@
 
 #include "librm/hal/serial.h"
 #include "librm/core/typedefs.h"
-#include "librm/modules/algorithm/crc32.h"
-#include "librm/modules/algorithm/crc_ccitt.h"
 
 #include <string>
 
-union com_data32 {
-  i32 L;
-  u8 ku8[4];
-  u16 ku16[2];
-  u32 ku32;
-  f32 F;
-};
-
-struct control_param {
-  f32 tau;
-  f32 vel;
-  f32 pos;
-  f32 kp;
-  f32 kd;
-};
-
-struct feedback_param {
-  u8 mode;
-  i8 temp;
-  u8 m_error;
-
-  f32 tau;
-  f32 vel;
-
-  i16 acc;
-  f32 pos;
-
-  f32 gyro[3];
-  f32 accel[3];
-};
-
-struct com_head {
-  u8 head[2]{0xFE, 0xEE};
-  u8 motor_id;
-  u8 reserved;
-};
-
-struct com_data_send {
-  u8 mode;
-  u8 modify_bit;
-  u8 read_bit;
-  u8 reserved;
-
-  com_data32 Modify;
-
-  i16 tau;
-  i16 vel;
-  i32 pos;
-  i16 kp;
-  i16 kd;
-
-  u8 LowHzMotorCmdIndex;
-  u8 LowHzMotorCmdByte;
-
-  com_data32 Res[1];
-} __attribute__((packed));
-
-struct com_data_recv {
-  u8 mode;
-  u8 read_bit;
-  i8 temp;
-  u8 m_error;
-
-  com_data32 read;
-  i16 tau;
-
-  i16 vel;
-  f32 low_vel;
-
-  i16 vel_ref;
-  f32 low_vel_ref;
-
-  i16 acc;
-  i16 out_acc;
-
-  i32 pos;
-  i32 out_pos;
-
-  i16 gyro[3];
-  i16 accel[3];
-
-  i16 f_gyro[3];
-  i16 f_acc[3];
-  i16 f_mag[3];
-  u8 f_temp;
-
-  i16 force16;
-  i8 force8;
-
-  u8 f_error;
-
-  i8 res[1];
-} __attribute__((packed));
-
-struct send_data {
-  com_head head;
-  com_data_send data;
-  com_data32 crc;
-} __attribute__((packed));
-
-struct recv_data {
-  com_head head;
-  com_data_recv data;
-  com_data32 crc;
-} __attribute__((packed));
-
 namespace rm::device {
+
 class UnitreeMotor {
+ public:
+  union ComData32 {
+    i32 L;
+    u8 ku8[4];
+    u16 ku16[2];
+    u32 ku32;
+    f32 F;
+  };
+
+  struct ControlParam {
+    f32 tau;
+    f32 vel;
+    f32 pos;
+    f32 kp;
+    f32 kd;
+  };
+
+  struct FeedbackParam {
+    u8 mode;
+    i8 temp;
+    u8 m_error;
+
+    f32 tau;
+    f32 vel;
+
+    i16 acc;
+    f32 pos;
+
+    f32 gyro[3];
+    f32 accel[3];
+  };
+
+  struct ComHead {
+    u8 head[2]{0xFE, 0xEE};
+    u8 motor_id;
+    u8 reserved;
+  };
+
+  struct ComDataSend {
+    u8 mode;
+    u8 modify_bit;
+    u8 read_bit;
+    u8 reserved;
+
+    ComData32 Modify;
+
+    i16 tau;
+    i16 vel;
+    i32 pos;
+    i16 kp;
+    i16 kd;
+
+    u8 LowHzMotorCmdIndex;
+    u8 LowHzMotorCmdByte;
+
+    ComData32 Res[1];
+  } __attribute__((packed));
+
+  struct ComDataReceive {
+    u8 mode;
+    u8 read_bit;
+    i8 temp;
+    u8 m_error;
+
+    ComData32 read;
+    i16 tau;
+
+    i16 vel;
+    f32 low_vel;
+
+    i16 vel_ref;
+    f32 low_vel_ref;
+
+    i16 acc;
+    i16 out_acc;
+
+    i32 pos;
+    i32 out_pos;
+
+    i16 gyro[3];
+    i16 accel[3];
+
+    i16 f_gyro[3];
+    i16 f_acc[3];
+    i16 f_mag[3];
+    u8 f_temp;
+
+    i16 force16;
+    i8 force8;
+
+    u8 f_error;
+
+    i8 res[1];
+  } __attribute__((packed));
+
+  struct SendData {
+    ComHead head;
+    ComDataSend data;
+    ComData32 crc;
+  } __attribute__((packed));
+
+  struct ReceiveData {
+    ComHead head;
+    ComDataReceive data;
+    ComData32 crc;
+  } __attribute__((packed));
+
  public:
   UnitreeMotor(hal::SerialInterface &serial, u8 motor_id = 0x0);
   ~UnitreeMotor() = default;
@@ -159,15 +160,15 @@ class UnitreeMotor {
   [[nodiscard]] f32 GetPos() { return this->fb_param_.pos; }
 
  private:
-  void SetParam(const control_param& ctrl_param);
+  void SetParam(const ControlParam &ctrl_param);
 
  private:
   hal::SerialInterface *serial_;
 
-  send_data send_data_;
-  recv_data recv_data_;
-  control_param ctrl_param_;
-  feedback_param fb_param_;
+  SendData send_data_;
+  ReceiveData recv_data_;
+  ControlParam ctrl_param_;
+  FeedbackParam fb_param_;
   u8 tx_buffer_[34]{0};
 };
 
